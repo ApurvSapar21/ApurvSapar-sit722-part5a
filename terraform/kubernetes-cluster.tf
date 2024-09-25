b9266070c48035c29b5a22cb01931d531e4fe6d4
Apurv <apurvsapar@gmail.com>
2024-09-26 02:02:04 +1000

    Update kubernetes-cluster.tf

--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -1,6 +1,3 @@
-#
-# Creates a managed Kubernetes cluster on Azure.
-#
 resource "azurerm_kubernetes_cluster" "cluster" {
     name                = var.app_name
     location            = var.location
@@ -14,21 +11,13 @@ resource "azurerm_kubernetes_cluster" "cluster" {
         vm_size         = "Standard_B2s"
     }
 
-    #
-    # Instead of creating a service principle have the system figure this out.
-    #
     identity {
         type = "SystemAssigned"
     }    
 }
 
-#
-# Attaches the container registry to the cluster.
-# See example here: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/container_registry#example-usage-attaching-a-container-registry-to-a-kubernetes-cluster
-#
 resource "azurerm_role_assignment" "role_assignment" {
-  principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.container_registry.id
-  skip_service_principal_aad_check = true
+  principal_id          = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+  role_definition_name  = "AcrPull"
+  scope                 = azurerm_container_registry.container_registry.id
 }
