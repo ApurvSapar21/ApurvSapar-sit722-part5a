986c206b5856c6e050b76aa6647584346133b806
Apurv <apurvsapar@gmail.com>
2024-09-27 22:32:27 +1000

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,16 +1,11 @@
-#providers
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.71.0"
-    }
-  }
-
-  required_version = ">= 1.5.6"
-}
-
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
+  subscription_id = "139404b5-6651-4f3f-81b5-1bdc19aa8af3"
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  client_certificate      = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key              = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate  = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
